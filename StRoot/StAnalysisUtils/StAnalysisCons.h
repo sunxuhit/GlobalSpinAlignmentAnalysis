3fff7c09dd6000dbcbd8e857973306cf55c899dd
Xu Sun <sunxuhit@gmail.com>
2023-01-16 03:01:14 -0500

    v2.09: update StRefMultCorr from STAR Lib

--- StRoot/StAnalysisUtils/StAnalysisCons.h
+++ StRoot/StAnalysisUtils/StAnalysisCons.h
@@ -3,7 +3,6 @@
 
 #include <string>
 #include "TString.h"
-// #include "StarClassLibrary/SystemOfUnits.h"
 
 namespace anaUtils
 {
@@ -12,8 +11,8 @@ namespace anaUtils
   const int mNumBeamUtils = 2; // 0 for ZrZr200GeV_2018, 1 for RuRu200GeV_2018, 2 for Fixed Target?
 
   // event cuts | copied from Isobar CME analysis
-  const float mVzMax[mNumBeamUtils]                  = {25.0, 25.0};
   const float mVzMin[mNumBeamUtils]                  = {-35.0, -35.0};
+  const float mVzMax[mNumBeamUtils]                  = {25.0, 25.0};
   const float mVrMax[mNumBeamUtils]                  = {2.0, 2.0};
   const float mVzVpdDiffMax[mNumBeamUtils]           = {5.0, 5.0};
   const unsigned short mMatchedToFMin[mNumBeamUtils] = {2, 2};
@@ -21,7 +20,7 @@ namespace anaUtils
   // track cuts: Basic 
   const int   mHitsFitTpcMin   = 15;
   const int   mHitsMaxTpcMin   = 0;
-  const float mHitsRatioTpcMin = 0.51;
+  const float mHitsRatioTpcMin = 0.52;
   // const int   mHitsDedxMin     = 5; // not used
 
   // track cuts: RunQA
@@ -29,18 +28,20 @@ namespace anaUtils
   const float mEtaQaMax    = 1.0;
   const float mPrimPtQaMin = 0.1; 
 
+  // track cuts: Event Plane Maker
+  // const float mDcaEpMax       = 3.0;
+  // const float mEtaEpMax       = 1.0;
   // const float mPrimPtEpMin    = 0.2; // for event plane reconstruction
   // const float mPrimPtEpMax    = 2.0;
   // const float mPrimPtEpWeight = 2.0;
   // const float mPrimMomEpMax   = 10.0;
-  // const float mDcaEpMax       = 3.0;
-  // const float mEtaEpMax       = 1.5;
   // const float mEtaEpGap       = 0.05;
 
-  // const float mPrimPtDauMin  = 0.1; // for phi decay daughter K+ and K-
-  // const float mPrimMomDauMax = 10.0;
+  // track cuts: phi Meson Maker
   // const float mDcaDauMax     = 3.0;
   // const float mEtaDauMax     = 1.0;
+  // const float mPrimPtDauMin  = 0.2; // for phi decay daughter K+ and K-
+  // const float mPrimMomDauMax = 10.0;
 }
 
 #endif
