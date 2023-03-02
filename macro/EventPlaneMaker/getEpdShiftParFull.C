7ef0089913eefc840af1ed28ca56d867367ecef0
Xu Sun <sunxuhit@gmail.com>
2023-03-02 09:42:06 +0800

    v3.22: update macros of EventPlaneMaker

--- macro/EventPlaneMaker/getEpdShiftParFull.C
+++ macro/EventPlaneMaker/getEpdShiftParFull.C
@@ -47,4 +47,49 @@ void getEpdShiftParFull(int beamType = 0)
     }
   }
   file_OutPut->Close();
+
+  // Event Plane Distribution | x axis is runIndex, y axis is EP angle
+  TH2F *h_mEpdEp1ShiftEast[mNumCentrality]; // 1st weighted EP
+  TH2F *h_mEpdEp1ShiftWest[mNumCentrality];
+  TH2F *h_mEpdEp1ShiftFull[mNumCentrality];
+  TH2F *h_mEpdEp1ShiftCorr[mNumCentrality]; // Psi1East vs Psi1West
+  for(int iCent = 0; iCent < mNumCentrality; ++iCent)
+  {
+    std::string histName = Form("h_mEpdEp1ShiftEastCent%d",iCent); // 1st EP
+    h_mEpdEp1ShiftEast[iCent] = (TH2F*)file_InPut->Get(histName.c_str());
+    histName = Form("h_mEpdEp1ShiftWestCent%d",iCent);
+    h_mEpdEp1ShiftWest[iCent] = (TH2F*)file_InPut->Get(histName.c_str());
+    histName = Form("h_mEpdEp1ShiftFullCent%d",iCent);
+    h_mEpdEp1ShiftFull[iCent] = (TH2F*)file_InPut->Get(histName.c_str());
+    histName = Form("h_mEpdEp1ShiftCorrCent%d",iCent);
+    h_mEpdEp1ShiftCorr[iCent] = (TH2F*)file_InPut->Get(histName.c_str());
+  }
+
+  TCanvas *c_EpdEp1ShiftDist[mNumCentrality];
+  for(int iCent = 0; iCent < mNumCentrality; ++iCent)
+  {
+    std::string canvName = Form("c_EpdEp1ShiftDistCent%d",iCent);
+    c_EpdEp1ShiftDist[iCent] = new TCanvas(canvName.c_str(),canvName.c_str(),10,10,1600,400);
+    c_EpdEp1ShiftDist[iCent]->Divide(4,1);
+    c_EpdEp1ShiftDist[iCent]->cd(1); h_mEpdEp1ShiftEast[iCent]->ProjectionY()->Draw();
+    c_EpdEp1ShiftDist[iCent]->cd(2); h_mEpdEp1ShiftWest[iCent]->ProjectionY()->Draw();
+    c_EpdEp1ShiftDist[iCent]->cd(3); h_mEpdEp1ShiftFull[iCent]->ProjectionY()->Draw();
+    c_EpdEp1ShiftDist[iCent]->cd(4); h_mEpdEp1ShiftCorr[iCent]->Draw("colz");
+
+    std::string figName = Form("../../figures/%s/EventPlaneMaker/EpdShiftEpCent%d_%s.pdf",globCons::str_mBeamType[beamType].c_str(),iCent,globCons::str_mBeamType[beamType].c_str());
+    c_EpdEp1ShiftDist[iCent]->SaveAs(figName.c_str());
+  }
+
+  string outputFileShiftEp = Form("../../data/%s/EventPlaneMaker/file_EpdShiftEpDist_%s.root",globCons::str_mBeamType[beamType].c_str(),globCons::str_mBeamType[beamType].c_str());
+  cout << "outputFile of Raw EP: " << outputFileShiftEp.c_str() << endl;
+  TFile *file_OutPutShiftEp = new TFile(outputFileShiftEp.c_str(),"RECREATE");
+  file_OutPutShiftEp->cd();
+  for(int iCent = 0; iCent < mNumCentrality; ++iCent)
+  {
+    h_mEpdEp1ShiftEast[iCent]->Write();
+    h_mEpdEp1ShiftWest[iCent]->Write();
+    h_mEpdEp1ShiftFull[iCent]->Write();
+    h_mEpdEp1ShiftCorr[iCent]->Write();
+  }
+  file_OutPutShiftEp->Close();
 }
--- macro/EventPlaneMaker/getZdcShiftParFull.C
+++ macro/EventPlaneMaker/getZdcShiftParFull.C
@@ -27,9 +27,9 @@ void getZdcShiftParFull(int beamType = 0)
   {
     for(int iShift = 0; iShift < mNumShiftCorr; ++iShift) // Shift Order
     {
-      std::string proName = Form("p_mZdcQShiftCos%dFullVz%d",iShift,iVz);
+      std::string proName = Form("p_mZdcQ1ShiftCos%dFullVz%d",iShift,iVz);
       p_mZdcQ1ShiftCosFull[iVz][iShift] = (TProfile2D*)file_InPut->Get(proName.c_str());
-      proName = Form("p_mZdcQShiftSin%dFullVz%d",iShift,iVz);
+      proName = Form("p_mZdcQ1ShiftSin%dFullVz%d",iShift,iVz);
       p_mZdcQ1ShiftSinFull[iVz][iShift] = (TProfile2D*)file_InPut->Get(proName.c_str());
     }
   }
@@ -47,4 +47,49 @@ void getZdcShiftParFull(int beamType = 0)
     }
   }
   file_OutPut->Close();
+
+  // Event Plane Distribution | x axis is runIndex, y axis is EP angle
+  TH2F *h_mZdcEp1ShiftEast[mNumCentrality]; // shift EP
+  TH2F *h_mZdcEp1ShiftWest[mNumCentrality];
+  TH2F *h_mZdcEp1ShiftFull[mNumCentrality]; // Qwest-QEast
+  TH2F *h_mZdcEp1ShiftCorr[mNumCentrality]; // Psi1East vs Psi1West
+  for(int iCent = 0; iCent < mNumCentrality; ++iCent)
+  {
+    std::string histName = Form("h_mZdcEp1ShiftEastCent%d",iCent);
+    h_mZdcEp1ShiftEast[iCent] = (TH2F*)file_InPut->Get(histName.c_str());
+    histName = Form("h_mZdcEp1ShiftWestCent%d",iCent);
+    h_mZdcEp1ShiftWest[iCent] = (TH2F*)file_InPut->Get(histName.c_str());
+    histName = Form("h_mZdcEp1ShiftFullCent%d",iCent);
+    h_mZdcEp1ShiftFull[iCent] = (TH2F*)file_InPut->Get(histName.c_str());
+    histName = Form("h_mZdcEp1ShiftCorrCent%d",iCent);
+    h_mZdcEp1ShiftCorr[iCent] = (TH2F*)file_InPut->Get(histName.c_str());
+  }
+
+  TCanvas *c_ZdcEp1ShiftDist[mNumCentrality];
+  for(int iCent = 0; iCent < mNumCentrality; ++iCent)
+  {
+    std::string canvName = Form("c_EpdEp1ShiftDistCent%d",iCent);
+    c_ZdcEp1ShiftDist[iCent] = new TCanvas(canvName.c_str(),canvName.c_str(),10,10,1600,400);
+    c_ZdcEp1ShiftDist[iCent]->Divide(4,1);
+    c_ZdcEp1ShiftDist[iCent]->cd(1); h_mZdcEp1ShiftEast[iCent]->ProjectionY()->Draw();
+    c_ZdcEp1ShiftDist[iCent]->cd(2); h_mZdcEp1ShiftWest[iCent]->ProjectionY()->Draw();
+    c_ZdcEp1ShiftDist[iCent]->cd(3); h_mZdcEp1ShiftFull[iCent]->ProjectionY()->Draw();
+    c_ZdcEp1ShiftDist[iCent]->cd(4); h_mZdcEp1ShiftCorr[iCent]->Draw("colz");
+
+    std::string figName = Form("../../figures/%s/EventPlaneMaker/ZdcShiftEpCent%d_%s.pdf",globCons::str_mBeamType[beamType].c_str(),iCent,globCons::str_mBeamType[beamType].c_str());
+    c_ZdcEp1ShiftDist[iCent]->SaveAs(figName.c_str());
+  }
+
+  string outputFileShiftEp = Form("../../data/%s/EventPlaneMaker/file_ZdcShiftEpDist_%s.root",globCons::str_mBeamType[beamType].c_str(),globCons::str_mBeamType[beamType].c_str());
+  cout << "outputFile of Shift EP: " << outputFileShiftEp.c_str() << endl;
+  TFile *file_OutPutShiftEp = new TFile(outputFileShiftEp.c_str(),"RECREATE");
+  file_OutPutShiftEp->cd();
+  for(int iCent = 0; iCent < mNumCentrality; ++iCent)
+  {
+    h_mZdcEp1ShiftEast[iCent]->Write();
+    h_mZdcEp1ShiftWest[iCent]->Write();
+    h_mZdcEp1ShiftFull[iCent]->Write();
+    h_mZdcEp1ShiftCorr[iCent]->Write();
+  }
+  file_OutPutShiftEp->Close();
 }
