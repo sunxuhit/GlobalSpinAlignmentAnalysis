9fcde6ae70962a40265f64b4e1cfdeee09b2cb8f
Xu Sun <sunxuhit@gmail.com>
2023-03-06 04:26:01 -0500

    v0.2_FxtMode: Fix collision par in getFileList.sh

--- macro/FileList/Fxt3p85GeV_2018/getFileList.sh
+++ macro/FileList/Fxt3p85GeV_2018/getFileList.sh
@@ -10,7 +10,7 @@ then
   rm $OutPutDir/pico_sorted.list
   rm $OutPutDir/runNumberPicoDst.list
 
-  get_file_list.pl -keys 'path,filename' -cond 'production=P19ie,library=SL20d,trgsetupname=production_3p85GeV_fixedTarget_2018,collision=Au3.85,filetype=daq_reco_picoDst,filename~st_physics,storage!=hpss' -limit 0 -delim / -distinct > $OutPutDir/pico.list
+  get_file_list.pl -keys 'path,filename' -cond 'production=P19ie,library=SL20d,trgsetupname=production_3p85GeV_fixedTarget_2018,collision=AuAu8,filetype=daq_reco_picoDst,filename~st_physics,storage!=hpss' -limit 0 -delim / -distinct > $OutPutDir/pico.list
 
   awk -F/ '{print $NF, $0}' $OutPutDir/pico.list | sort | uniq | cut -f2- -d ' ' > $OutPutDir/pico_sorted.list
   awk -F/ '{print $(NF-1), $0}' $OutPutDir/pico.list | cut -d ' ' -f 1 | sort | uniq > $OutPutDir/runNumberPicoDst.list
