fc0a190f9ba5baa80b7d644b202cb9b4862841b7
Xu Sun <sunxuhit@gmail.com>
2023-03-06 17:54:45 +0800

    v0.3_FxtMode: implement RunIndex for Fxt Mode

--- Utility/include/StSpinAlignmentCons.h
+++ Utility/include/StSpinAlignmentCons.h
@@ -5,13 +5,13 @@
 
 namespace globCons
 {
-  const int mNumBeamType                        = 2; // 0 for ZrZr200GeV_2018, 1 for RuRu200GeV_2018, 2 for Fixed Target?
-  const std::string str_mBeamType[mNumBeamType] = {"ZrZr200GeV_2018", "RuRu200GeV_2018"};
-  const double mBeamEnergy[mNumBeamType]        = {200.0, 200.0};
-  const int mBeamYear[mNumBeamType]             = {2018, 2018};
-  const int mMaxRunIndex[mNumBeamType]          = {1500, 1500}; // maximum number of runIndex
-  const int mRunIndexLo[mNumBeamType]           = {0,    2500}; 
-  const int mRunIndexHi[mNumBeamType]           = {1500, 4000};
+  const int mNumBeamType                        = 3; // 0 for ZrZr200GeV_2018, 1 for RuRu200GeV_2018, 2 for Fxt3p85GeV_2018
+  const std::string str_mBeamType[mNumBeamType] = {"ZrZr200GeV_2018", "RuRu200GeV_2018", "Fxt3p85GeV_2018"};
+  const double mBeamEnergy[mNumBeamType]        = {200.0, 200.0, 3.85};
+  const int mBeamYear[mNumBeamType]             = {2018, 2018, 2018};
+  const int mMaxRunIndex[mNumBeamType]          = {1500, 1500, 300}; // maximum number of runIndex
+  const int mRunIndexLo[mNumBeamType]           = {0,    2500, 0}; 
+  const int mRunIndexHi[mNumBeamType]           = {1500, 4000, 300};
 }
 
 #endif
