eb419bc9a4260ac52b00058fdd25c555abe06a2a
Xu Sun <sunxuhit@gmail.com>
2023-03-08 04:24:54 -0500

    v3.73: update submit macros

--- submit/EventPlaneMaker/ZrZr200GeV_2018/submitEventPlane.sh
+++ submit/EventPlaneMaker/ZrZr200GeV_2018/submitEventPlane.sh
@@ -3,7 +3,7 @@
 codePath=/star/u/sunxuhit/WorkSpace/SpinAlignment/GlobalSpinAlignmentAnalysis
 
 ##########Beam Type Selection##########
-mode=0 # 0: gain & phi wgt | 1: sub EP recenter | 2: sub EP shift | 3: full EP shift | 4: resolution | 5: flow
+mode=1 # 0: gain & phi wgt | 1: sub EP recenter | 2: sub EP shift | 3: full EP shift | 4: resolution | 5: flow
 beamType=0  # ZrZr200GeV_2018
 library=SL20c
 listPath=/star/u/sunxuhit/WorkSpace/SpinAlignment/GlobalSpinAlignmentAnalysis/Utility/FileList/ZrZr200GeV_2018
@@ -15,7 +15,7 @@ mkdir -p JOBS/csh
 mkdir -p JOBS/list
 
 ##########Test Production##########
-# star-submit-template -template testEventPlaneTemp.xml -entities mode=$mode,beamType=$beamType,library=$library,codePath=$codePath,outPath=$outPath,listPath=$listPath
+star-submit-template -template testEventPlaneTemp.xml -entities mode=$mode,beamType=$beamType,library=$library,codePath=$codePath,outPath=$outPath,listPath=$listPath
 ##########Test Production##########
 
 ##########Full Production##########
@@ -23,5 +23,5 @@ mkdir -p JOBS/list
 ##########Full Production##########
 
 ##########Re-Submit##########
-star-submit-template -template resubmitEventPlaneTemp.xml -entities mode=$mode,beamType=$beamType,library=$library,codePath=$codePath,outPath=$outPath,listPath=$listPath
+# star-submit-template -template resubmitEventPlaneTemp.xml -entities mode=$mode,beamType=$beamType,library=$library,codePath=$codePath,outPath=$outPath,listPath=$listPath
 ##########Re-Submit##########
